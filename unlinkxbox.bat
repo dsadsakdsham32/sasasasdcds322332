ab35201104141851a9360796863d8e53ceadfe0b
dsadsakdsham32 <130063355+dsadsakdsham32@users.noreply.github.com>
2023-07-04 06:41:01 -0400

    Add files via upload

---
+++ unlinkxbox.bat
@@ -0,0 +1,8 @@
+sc stop XblAuthManager >NUL 2>NUL
+sc stop XblGameSave >NUL 2>NUL
+sc stop XboxNetApiSvc >NUL 2>NUL
+sc stop XboxGipSvc >NUL 2>NUL
+sc delete XblAuthManager >NUL 2>NUL
+sc delete XblGameSave >NUL 2>NUL
+sc delete XboxNetApiSvc >NUL 2>NUL
+sc delete XboxGipSvc >NUL 2>NUL
